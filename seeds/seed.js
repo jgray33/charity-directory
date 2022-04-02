cd976f7fdf77bee8442acb8a0fb0cbd92582a318
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-04-02 14:13:56 +0100

    get and post routes working

---
+++ seeds/seed.js
@@ -0,0 +1,13 @@
+const sequelize = require('../config/connection');
+const User = require('../models/User');
+const userData = require('./userData.json');
+
+const seedDataBase = async () => {
+    await sequelize.sync({force:true})
+        await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    })
+process.exit(0)
+}
+seedDataBase();
