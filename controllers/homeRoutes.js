c737af64b330108c3d3110ed1b1591d51eeba30a
Julia <julia.gray30@gmail.com>
2022-04-09 22:33:00 +0100

    login fixed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,142 +1,39 @@
-const router = require("express").Router()
-const sequelize = require('../config/connection');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
 const Charity = require("../models/Charity");
 const User = require("../models/User");
 
-router.get("/", async (req,res) => {
-   res.render ('homepage', {
-     
-   })
-})
+router.get("/", async (req, res) => {
+  res.render("homepage", {});
+});
 
-router.get("/login", async (req,res) => {
-   res.render ('login', {
-     
-   })
-})
-
-router.get("/get-involved", async (req,res) => {
-  res.render ('getInvolved', {
-    
-  })
-})
-
-router.get("/charity", async (req,res) => {
-  try{
-      const charityData = await Charity.findAll()
-      const profiles = charityData.map((profile) => profile.get({plain:true}))
-              res.render("charitypage", {profiles})
-          } catch (err) {
-      res.status(500).json(err)
-      console.log(err)
-  }
-})
-
-
-router.get("/:charity_name", async(req,res) => {
-  try {
-      const charityData =  await Charity.findOne({
-          where: { charity_name: req.params.charity_name },
-      })
-      if(!charityData){
-          res.status(404).json({message: "no charity under that name"})
-          return
-      }
-      res.status(200).json(charityData)
-      } catch (err){
-          res.status(500).json(err)
-          console.log(err)
-      }})
-
-
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
-  res.render('login');
+  res.render("login");
 });
 
-router.get('/signup', (req, res) => {
-  res.render('signup');
+router.get("/get-involved", async (req, res) => {
+  res.render("getInvolved", {});
 });
 
-router.get('/post/:id', (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: ['id', 'content', 'title', 'created_at'],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username'],
-        },
-      },
-      {
-        model: User,
-        attributes: ['username'],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-      const post = dbPostData.get({ plain: true });
-      res.render('single-post', {
-        post,
-        logged_in: req.session.logged_in,
-        username: req.session.username,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+router.get("/all", async (req, res) => {
+  try {
+    const charityData = await Charity.findAll();
+    const profiles = charityData.map((profile) => profile.get({ plain: true }));
+    res.render("charitypage", { profiles });
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 });
-router.get('/posts-comments', (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: ['id', 'content', 'title', 'created_at'],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username'],
-        },
-      },
-      {
-        model: User,
-        attributes: ['username'],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-      const post = dbPostData.get({ plain: true });
 
-      res.render('posts-comments', {
-        post,
-        logged_in: req.session.logged_in,
-        username: req.session.username,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
 module.exports = router;
--- public/js/home.js
+++ public/js/home.js
@@ -4,16 +4,28 @@ console.log("connected to home js")
 const searchBar = document.getElementById("searchBar")
 const searchBtn = document.getElementById("searchBtn")
 
-function searchCharity(event) {
+async function searchCharity(event) {
     const searchValue = searchBar.value.trim()
     event.preventDefault()
     console.log("clicked", searchValue)
-    const requestUrl = `/charity/${searchValue}`
-    console.log(requestUrl)
 
-    const response = await fetch(requestUrl)
-    let data = await response.json()
-    console.log(data)
-}
+    const response = await fetch("/api/charity/:charity_name", {
+        method: "GET",
+        body: JSON.stringify({
+            searchValue
+        }),
+        headers: { "Content-Type": "application/json" },
+        })
+                if (response.ok) {
+                    
+                console.log("That worked")
+            } else {
+                console.log("That did not work ")
+                console.log(err)
+            }}
+    
+   
+
+  
 
 searchBtn.addEventListener("click", searchCharity)
