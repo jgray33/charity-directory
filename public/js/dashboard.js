8fe6cd8a724a0c63b8167bc39f436cb9a7d4eed7
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-04-14 18:37:28 +0100

    dashboard created

---
+++ public/js/dashboard.js
@@ -0,0 +1,13 @@
+const title = document.querySelector("#title-register");
+const contents = document.querySelector("#contents-register");
+const dashBtn = document.querySelector("#dashboard-btn");
+
+function dashboard() {
+  console.log(
+    title.value,
+    contents.value
+  );
+}
+
+
+btn.addEventListener("click", dashboard);
